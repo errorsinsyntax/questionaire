5edeacf5b88ef3e4721cba548f702c8ae0279041
Crys <errorsinsyntax@gmail.com>
2020-11-10 15:48:32 -0500

    finished game without questions

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ const quizData = [
     
 ];
 
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll(".answer");
 const quesitonEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -39,12 +41,13 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
-    const currentQuizData = quizData
-    [currentQuiz];
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerHTML = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -54,11 +57,43 @@ function loadQuiz(){
 
 }
 
+function getSelected(){
+    
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    })
+};
+
 submitBtn.addEventListener('click', () => {
-    currentQuiz++;
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        alert('fin!');
+    //check answer
+    const answer = getSelected();
+
+    console.log(answer)
+
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+            currentQuiz++;
+            if(currentQuiz < quizData.length) {
+                loadQuiz();
+            } else {
+                //show results here
+                quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions, correctly.</h2><button onclick='location.reload()'>Play Again</button>`;
+                
+            }
+        }
     }
-});
+);
